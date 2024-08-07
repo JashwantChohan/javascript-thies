f323830eb69208ef68e1a9ddab306c54af9ff86e
Jashwant Chohan <jchohan142@gmial.com>
2024-08-07 06:33:10 -0700

    first commit

---
+++ function.js
@@ -0,0 +1,24 @@
+// function myclass(){
+//     let laptops =2+3;
+//     console.log(laptops);
+    
+// }
+
+// myclass()
+
+const num1 = 200;
+const num2 = 3;
+const name = "Jashwant";
+
+function getscore() {
+    const num1 = 2;
+    const num2 = 3;
+
+    function add(){
+        console.log(name + " scored " + `${num1 + num2}`)
+    }
+return add();
+
+}
+
+console.log(getscore());
